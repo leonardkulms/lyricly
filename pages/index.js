ea428c55dda51d15a2b7f8e0b17188d42f18a1b7
Leonard Kulms <leonard.kulms@icloud.com>
2021-08-02 08:57:33 +0200

    integrate genius api

--- pages/index.js
+++ pages/index.js
@@ -2,21 +2,31 @@ import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 import { useEffect, useState } from 'react';
-import { spotifyWebApiURL } from '../constants/'
+import { geniusWebApiURL, spotifyWebApiURL } from '../constants/'
 import Router from 'next/router'
 
 export default function Home() {
-  const NOW_PLAYING_ENDPOINT = `https://api.spotify.com/v1/me/player/currently-playing`;
-  const TOP_TRACKS_ENDPOINT = `https://api.spotify.com/v1/me/top/tracks`;
-  const TOKEN_ENDPOINT = `https://accounts.spotify.com/api/token`;
-
   const [accessToken, setAccessToken] = useState('');
+  const [geniusAccessToken, setGeniusAccessToken] = useState('');
 
-  function handleClick() {
+
+  function handleSpotifyClick() {
     document.location = spotifyWebApiURL
   }
 
-  function setToken() {
+  function setSpotifyToken() {
+    let url = window.location.href;
+    if (url.indexOf('_token') > -1) {
+      setAccessToken(url.split('_token=')[1].split("&")[0].trim())
+    }
+  }
+
+
+  function handleGeniusClick() {
+    document.location = geniusWebApiURL
+  }
+
+  function setGeniusToken() {
     let url = window.location.href;
     if (url.indexOf('_token') > -1) {
       setAccessToken(url.split('_token=')[1].split("&")[0].trim())
@@ -24,7 +34,8 @@ export default function Home() {
   }
 
   useEffect(() => {
-    setToken();
+    setSpotifyToken();
+    setGeniusAccessToken();
   }, []);
 
   return (
@@ -40,8 +51,11 @@ export default function Home() {
         <h1 className={styles.title}>
           Lyricly
         </h1>
-        <button onClick={() => handleClick()}>
+        <button onClick={() => handleSpotifyClick()}>
           Connect to Spotify
+        </button>
+        <button onClick={() => handleGeniusClick()}>
+          Connect to Genius
         </button>
         <button onClick={event => Router.push({
           pathname: '/app',
